b05f753d3bca87ff0bb0d36a133f1fd02caaaba2
aron-harchpro <46327525+webdevarif@users.noreply.github.com>
2024-11-02 11:14:48 +0600

    Update

--- src/components/Card/CardProject.tsx
+++ src/components/Card/CardProject.tsx
@@ -15,7 +15,7 @@ const CardProject: React.FC<{ post: ProjectPostProps }> = ({ post }) => {
     <Card className='h-full relative flex flex-col border border-black before:transition-all before:duration-300 before:ease-linear before:content-[""] before:w-full before:h-full before:bg-black group/item before:absolute before:end-0 before:z-[-1] before:translate-y-0 hover:before:-end-[0.35rem] hover:before:translate-y-[0.35rem]'>
       {post.featured_image && (
         <div className="w-full h-[15rem] overflow-hidden">
-          <Image className='w-full h-full group-hover/item:scale-105 duration-300 transition-all ease-linear object-cover object-[top_center] rounded-none' alt={post.title || ''} src={post.featured_image} width={400} height={300} />
+          <Image className='w-full h-full group-hover/item:object-[0%_100%] duration-5000 transition-all ease-linear object-cover object-[0%_0%] rounded-none' alt={post.title || ''} src={post.featured_image} width={400} height={300} />
         </div>
       )}
       <CardHeader>
