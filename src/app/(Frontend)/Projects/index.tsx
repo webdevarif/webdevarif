c46c1dbe6467df1bcd25bdb375719ab1f142b0ea
aron-harchpro <46327525+webdevarif@users.noreply.github.com>
2024-11-02 12:18:55 +0600

    Update index.tsx

--- src/app/(Frontend)/Projects/index.tsx
+++ src/app/(Frontend)/Projects/index.tsx
@@ -7,12 +7,14 @@ import Link from 'next/link';
 
 const Projects = () => {
     const { data, isLoading } =useProjects(`current-page=1&per-page=6`);
+
   return (
     <section className="py-[100px]">
         <div className="container">
+
             <div className="mb-[3rem] mx-auto text-center max-w-[40rem]">
-            <span className="uppercase font-unbounded font-bold text-4xl mb-3 inline-block">Recent Projects</span>
-            <p className="font-medium">Valuable insights to change your startup idea</p>
+                <span className="uppercase font-unbounded font-bold text-4xl mb-3 inline-block">Recent Projects</span>
+                <p className="font-medium">Valuable insights to change your startup idea</p>
             </div>
 
             <div className="grid grid-cols-3 gap-4">
@@ -26,7 +28,7 @@ const Projects = () => {
             </div>
             <div className="text-center mt-[2rem]">
                 <Button variant={'default'}>
-                    <Link href={'/projects'} className='min-w-[8rem] text-md uppercase h-auto py-3 inline-block max-w-full'>View All</Link>
+                    <Link href={'/projects'} className='min-w-[8rem] text-md uppercase h-auto py-4 inline-block max-w-full'>View All</Link>
                 </Button>
             </div>
         </div>
