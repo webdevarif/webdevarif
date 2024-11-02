a0b8c41b3c84f59b0765f035b883a6b4ee71f439
aron-harchpro <46327525+webdevarif@users.noreply.github.com>
2024-11-02 11:53:09 +0600

    Update CardProject.tsx

--- src/components/Card/CardProject.tsx
+++ src/components/Card/CardProject.tsx
@@ -14,12 +14,12 @@ const CardProject: React.FC<{ post: ProjectPostProps }> = ({ post }) => {
   return (
     <Card className='h-full relative flex flex-col border border-black before:transition-all before:duration-300 before:ease-linear before:content-[""] before:w-full before:h-full before:bg-black group/item before:absolute before:end-0 before:z-[-1] before:translate-y-0 hover:before:-end-[0.35rem] hover:before:translate-y-[0.35rem]'>
       {post.featured_image && (
-          <Link href={post.preview_link} className="block w-full h-[15rem] overflow-hidden">
+          <Link href={post.preview_link} className="block w-full h-[15rem] overflow-hidden" target='_blank'>
             <Image className='w-full h-full group-hover/item:object-[0%_100%] duration-5000 transition-all ease-linear object-cover object-[0%_0%] rounded-none' alt={post.title || ''} src={post.featured_image} width={400} height={300} />
         </Link>
       )}
       <CardHeader>
-        <Link href={post.preview_link} className='text-black transition-all duration-300 hover:text-primary'>
+        <Link href={post.preview_link} className='text-black transition-all duration-300 hover:text-primary' target='_blank'>
           <h2 className="font-unbounded text-sm leading-[1.6] font-medium mb-0">{post.title}</h2>
         </Link>
       </CardHeader>
@@ -30,7 +30,7 @@ const CardProject: React.FC<{ post: ProjectPostProps }> = ({ post }) => {
           { post.project_categories && post.project_categories.map((category, index) => {
             // Return something for each category
             return (
-              <span key={index} className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ${Config.projectCategoryClass(category.name)}`}>
+              <span key={index} className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium uppercase ring-1 ring-inset ${Config.projectCategoryClass(category.name)}`}>
                 {category.name}
               </span>
             );
@@ -39,7 +39,7 @@ const CardProject: React.FC<{ post: ProjectPostProps }> = ({ post }) => {
       </CardContent>
       <CardFooter className='mt-auto border-t border-black pt-5'>
         <Button variant={'link'} asChild className='p-0 text-slate-black hover:text-primary h-auto inline-flex items-center gap-1 uppercase'>
-          <Link href={post.preview_link}>
+          <Link href={post.preview_link} target='_blank'>
             <span>Preview</span>
             <MdDoubleArrow />
           </Link>
